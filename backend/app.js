d0cc65643268fc240ce2910031d8063e9d3c184f
chrzrdx <chrzrdx@gmail.com>
2019-04-17 22:26:05 +0530

    initial

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+// importing the express library to serve the API's
+const express = require("express");
+// using request library to fetch the information from the pokeapi
+const app = express();
+// importing data.json file to get the pokemon details
+const pokemons = require("./data.json");
+
+// GET /api/pokemon/:id
+app.get("/api/pokemon/:id", (req, res) => {
+  if (!pokemons[req.params.id]) {
+    res.status(404).send({ error: "Pokemon not found" });
+    return;
+  }
+
+  var result = {
+    pokemon: {
+      id: req.params.id,
+      name: pokemons[req.params.id].name,
+      sprite: pokemons[req.params.id].sprite
+    }
+  };
+
+  res.send(result);
+});
+
+app.listen(8006, () => {
+  console.log("listening on port 8006");
+});
